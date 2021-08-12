f0823392f4335128c3b0671747f34e69d132f1ae
fabcodingzest <fabrizvi786@gmail.com>
2021-08-12 18:57:10 +0530

    experimenting with slate in frontend

--- components/RichText/slate-types.ts
+++ components/RichText/slate-types.ts
@@ -1,3 +1,7 @@
+import { ReactEditor } from 'slate-react';
+import { BaseEditor } from 'slate';
+import { HistoryEditor } from 'slate-history';
+
 export interface SlateText extends Text {
   bold?: boolean;
   code?: boolean;
@@ -7,9 +11,20 @@ export interface SlateText extends Text {
   text?: string;
 }
 
+type CustomText = {
+  text: string;
+  bold?: boolean;
+  italic?: boolean;
+  strikethrough?: boolean;
+  underline?: boolean;
+};
+export type CustomElement = { type: 'paragraph'; children: CustomText[] };
+
 // this is where you customize. If you omit this, it will revert to a default.
 declare module 'slate' {
   export interface CustomTypes {
+    Editor: BaseEditor & ReactEditor & HistoryEditor;
+    Element: CustomElement;
     Text: SlateText;
   }
 }
--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Slate, Editable, withReact } from 'slate-react';
+import { createEditor, Descendant } from 'slate';
 
-function hello(): JSX.Element {
+const Hello: React.FC = () => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState<Descendant[]>([
+    {
+      type: 'paragraph',
+      children: [
+        {
+          text: 'Hello',
+          bold: true,
+        },
+      ],
+    },
+  ]);
+
   return (
     <div>
       Hello
-      <p>From Fab</p>
+      <form>
+        <label htmlFor="product-name">Product Name</label>
+        <input type="text" id="product-name" />
+        <Slate
+          editor={editor}
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        >
+          <Editable
+            onKeyDown={(event) => {
+              if (event.key === '&') {
+                // Prevent the ampersand character from being inserted.
+                event.preventDefault();
+                // Execute the `insertText` method when the event occurs.
+                editor.insertText('and');
+              }
+            }}
+          />
+        </Slate>
+      </form>
     </div>
   );
-}
+};
 
-export default hello;
+export default Hello;
