42ea81062188490823b3872cacab9fae9e654e68
fabcodingzest <fabrizvi786@gmail.com>
2021-08-12 18:57:46 +0530

    adding utility functions for validation for positive non-decimal number

---
+++ utilities/validatePositiveNumber.ts
@@ -0,0 +1,7 @@
+export const validatePositiveNumber = (val: unknown): true | string => {
+  const str = val.toString();
+  const regex = /^[+]?([0-9]+\.[0-9]+)$/g;
+  const isNonDecimalPositiveInteger = regex.test(str);
+  if (isNonDecimalPositiveInteger) return true;
+  return 'The quantity should only be an integer and less than 10!';
+};
