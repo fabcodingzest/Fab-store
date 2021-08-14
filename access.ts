c5bef3f995b7c53512e7fe80b24a303a59e260bd
fabcodingzest <fabrizvi786@gmail.com>
2021-08-14 15:15:52 +0530

    changing functions arguments

--- access.ts
+++ access.ts
@@ -8,14 +8,14 @@ export const permissionsList = [
   'canManageOrders',
 ];
 
-export function isSignedIn(user): boolean {
+export function isSignedIn({ req: { user } }): boolean {
   return !!user;
 }
 
 const generatedPermissions = Object.fromEntries(
   permissionsList.map((permission) => [
     permission,
-    function (user) {
+    function ({ req: { user } }) {
       return !!user?.role?.[permission];
     },
   ])
@@ -29,56 +29,66 @@ export const permissions = {
 // Rule based function
 // Rules can return a boolean - yes or no - or a filter which limits which products they can CRUD.
 export const rules = {
-  canManageProducts: ({ req: { user } }): boolean | Record<string, unknown> => {
-    if (!isSignedIn(user)) {
+  canManageProducts: (data): boolean | Record<string, unknown> => {
+    const {
+      req: { user },
+    } = data;
+    if (!isSignedIn(data)) {
       return false;
     }
     // 1. Do they have the permission of canManageProducts
-    if (permissions.canManageProducts(user)) {
+    if (permissions.canManageProducts(data)) {
       return true;
     }
     // 2. If not, do they own this item?
     return { user: { id: user.id } };
   },
-  canOrder: ({ req: { user } }): boolean | Record<string, unknown> => {
-    if (!isSignedIn(user)) {
+  canOrder: (data): boolean | Record<string, unknown> => {
+    const {
+      req: { user },
+    } = data;
+    if (!isSignedIn(data)) {
       return false;
     }
     // 1. Do they have the permission of canManageProducts
-    if (permissions.canManageCart(user)) {
+    if (permissions.canManageCart(data)) {
       return true;
     }
     // 2. If not, do they own this item?
     return { user: { id: user.id } };
   },
-  canManageOrderItems: ({
-    req: { user },
-  }): boolean | Record<string, unknown> => {
-    if (!isSignedIn(user)) {
+  canManageOrderItems: (data): boolean | Record<string, unknown> => {
+    const {
+      req: { user },
+    } = data;
+    if (!isSignedIn(data)) {
       return false;
     }
     // 1. Do they have the permission of canManageProducts
-    if (permissions.canManageCart(user)) {
+    if (permissions.canManageCart(data)) {
       return true;
     }
     // 2. If not, do they own this item?
     return { order: { user: { equals: user.id } } };
   },
-  canReadProducts: ({ req: { user } }): boolean | Record<string, unknown> => {
-    if (!isSignedIn(user)) {
+  canReadProducts: (data): boolean | Record<string, unknown> => {
+    if (!isSignedIn(data)) {
       return false;
     }
-    if (permissions.canManageProducts(user)) {
+    if (permissions.canManageProducts(data)) {
       return true; // They can read everything!
     }
     // They should only see available products (based on the status field)
     return { variants: [{ status: { equals: 'AVAILABLE' } }] };
   },
-  canManageUsers: ({ req: { user } }): boolean | Record<string, unknown> => {
-    if (!isSignedIn(user)) {
+  canManageUsers: (data): boolean | Record<string, unknown> => {
+    const {
+      req: { user },
+    } = data;
+    if (!isSignedIn(data)) {
       return false;
     }
-    if (permissions.canManageUsers(user)) {
+    if (permissions.canManageUsers(data)) {
       return true;
     }
     // Otherwise they may only update themselves!
