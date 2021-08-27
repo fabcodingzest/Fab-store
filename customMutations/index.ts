e987938d16f587dd4635e34058e517121adc807d
fabcodingzest <fabrizvi786@gmail.com>
2021-08-27 23:52:35 +0530

    adding index file for custom mutations

---
+++ customMutations/index.ts
@@ -0,0 +1,24 @@
+/* eslint-disable dot-notation */
+import { addToCartResolver } from './addToCartResolver';
+import { checkoutResolver } from './checkout';
+
+export const customMutations = (GraphQL, payload) => ({
+  addToCart: {
+    type: payload.collections['cart_items'].graphQL.type,
+    args: {
+      productId: {
+        type: new GraphQL.GraphQLNonNull(GraphQL.GraphQLID),
+      },
+    },
+    resolve: addToCartResolver,
+  },
+  checkout: {
+    type: payload.collections['orders'].graphQL.type,
+    args: {
+      token: {
+        type: new GraphQL.GraphQLNonNull(GraphQL.GraphQLString),
+      },
+    },
+    resolve: checkoutResolver,
+  },
+});
