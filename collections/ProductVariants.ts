4cddafcad88527176fbbca143563c357a8a21090
fabcodingzest <fabrizvi786@gmail.com>
2021-08-31 13:31:19 +0530

    removing category from product

--- collections/ProductVariants.ts
+++ collections/ProductVariants.ts
@@ -8,25 +8,14 @@ export const ProductVariants: CollectionConfig = {
   },
   fields: [
     {
-      name: 'category',
-      label: 'Product Category',
-      type: 'select',
-      options: [
-        { label: 'Clothes', value: 'CLOTHES' },
-        { label: 'Stationary', value: 'STATIONARY' },
-        { label: 'Toys', value: 'TOYS' },
-        { label: 'Furniture', value: 'FURNITURE' },
-        { label: 'Books', value: 'BOOKS' },
-        { label: 'Jwellery', value: 'JWELLERY' },
-        { label: 'Electronics', value: 'ELECTRONICS' },
-      ],
-      required: true,
-      index: true,
-    },
-    {
       name: 'name',
       type: 'text',
       required: true,
+    },
+    {
+      name: 'parent',
+      type: 'relationship',
+      relationTo: 'products',
     },
     ...vairantFields,
   ],
