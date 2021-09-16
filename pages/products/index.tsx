989eb5adfec2537d3d2a75cb25cb287dfdf158e6
fabcodingzest <fabrizvi786@gmail.com>
2021-09-16 16:46:21 +0530

    Moving product to folder

---
+++ pages/products/index.tsx
@@ -0,0 +1,11 @@
+import ProductList from '../../components/Products/ProductList';
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <ProductList />
+    </div>
+  );
+};
+
+export default ProductsPage;
