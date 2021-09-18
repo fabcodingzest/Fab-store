0ca0f3b0fe6830950e7b69209b1a6344cbe5f5de
fabcodingzest <fabrizvi786@gmail.com>
2021-09-19 00:42:00 +0530

    creating modal and adding functionality

---
+++ components/Authentication/Modal.tsx
@@ -0,0 +1,61 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+  useColorModeValue,
+  Button,
+} from '@chakra-ui/react';
+import { useDisclosure } from '@chakra-ui/hooks';
+import { useState } from 'react';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import ForgotPassword from './ForgotPassword';
+
+const AuthModal = ({ modalState }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [modal, setModal] = useState(modalState);
+
+  return (
+    <>
+      <Button
+        size="sm"
+        py={{ base: 4, sm: 5 }}
+        px={{ base: 4, sm: 6 }}
+        onClick={onOpen}
+        colorScheme="blue"
+      >
+        {modalState === 'signin' ? 'Sign In' : 'Sign Up'}
+      </Button>
+
+      <Modal
+        size="lg"
+        isOpen={isOpen}
+        onClose={onClose}
+        onEsc={() => setModal(modalState)}
+        onOverlayClick={() => setModal(modalState)}
+        closeOnEsc
+        closeOnOverlayClick
+      >
+        <ModalOverlay />
+        <ModalContent bg={useColorModeValue('gray.50', 'gray.800')}>
+          <ModalBody>
+            {modal === 'signin' ? (
+              <SignIn setModalState={setModal} onClose={onClose} />
+            ) : modal === 'signup' ? (
+              <SignUp setModalState={setModal} onClose={onClose} />
+            ) : (
+              modal === 'forgotpassword' && (
+                <ForgotPassword setModalState={setModal} onClose={onClose} />
+              )
+            )}
+          </ModalBody>
+          <ModalCloseButton onClick={() => setModal(modalState)} />
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default AuthModal;
