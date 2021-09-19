96937aa20a7913fe9bc196619f191b673bf0b38f
fabcodingzest <fabrizvi786@gmail.com>
2021-09-19 17:56:19 +0530

    adding check for being logged in

--- pages/resetpassword.tsx
+++ pages/resetpassword.tsx
@@ -17,6 +17,7 @@ import React, { useEffect } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import FormValidationMsg from '../components/Authentication/FormValidationMsg';
 import Modal from '../components/Authentication/Modal';
+import { useUser } from '../components/User';
 
 type FormValues = {
   password: string;
@@ -53,11 +54,14 @@ const ResetPasswordPage = () => {
   });
   const password = watch('password');
   const { query } = useRouter();
-  console.log(query);
+  const bgHook = useColorModeValue('white', 'gray.700');
 
+  const me = useUser();
   useEffect(() => {
-    setFocus('password');
-  }, [setFocus]);
+    if (!me) {
+      setFocus('password');
+    }
+  }, [setFocus, me]);
 
   const [reset] = useMutation(RESET_PASSWORD_MUTATION);
 
@@ -101,90 +105,89 @@ const ResetPasswordPage = () => {
     }
   };
 
-  return (
-    <Flex align="center" justifyContent="center" minH="80vh">
-      <Flex minH="60vh" align="center" justify="center" borderRadius="2xl">
-        <Stack spacing={8} mx="auto" maxW="lg" py={10} px={6}>
-          <Stack align="center">
-            <Heading fontSize="3xl" align="center">
-              Forgot your password?
-            </Heading>
-            <Text fontSize="lg" color="gray.600">
-              You'll get an email with a reset link
-            </Text>
-          </Stack>
-          <Box
-            rounded="lg"
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow="lg"
-            p={8}
-          >
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Stack spacing={6}>
-                <FormControl id="password">
-                  <FormLabel htmlFor="password">New Password</FormLabel>
-                  <Input
-                    autoComplete="new-password"
-                    type="password"
-                    {...register('password', {
-                      required: 'Password is required',
-                      minLength: {
-                        value: 8,
-                        message: 'Password must be at least 8 characters long',
-                      },
-                      pattern: {
-                        value:
-                          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
-                        message:
-                          'Must Contain One Uppercase, One Lowercase, One Number and One Special Case Character',
-                      },
-                    })}
-                  />
-                  {errors.password && (
-                    <FormValidationMsg errorMsg={errors.password.message} />
-                  )}
-                </FormControl>
-                <FormControl id="passwordRepeat">
-                  <FormLabel htmlFor="passwordRepeat">
-                    Repeat New Password
-                  </FormLabel>
-                  <Input
-                    autoComplete="new-password"
-                    type="password"
-                    {...register('passwordRepeat', {
-                      required: 'Password is required',
-                      validate: {
-                        matches: (value: string) =>
-                          value === password || 'Passwords do not match',
-                      },
-                    })}
-                  />
-                  {errors.passwordRepeat && (
-                    <FormValidationMsg
-                      errorMsg={errors.passwordRepeat.message}
-                    />
-                  )}
-                </FormControl>
+  if (me)
+    return (
+      <Heading align="center" mt={12}>
+        You are already logged in BITCH! This for those who forgot the password.
+        Go to your account settings to change your password.
+      </Heading>
+    );
 
-                <Button
-                  type="submit"
-                  bg="blue.400"
-                  color="white"
-                  _hover={{
-                    bg: 'blue.500',
-                  }}
-                  disabled={!isValid || !isDirty || isSubmitting}
-                  isLoading={isSubmitting}
-                  loadingText="Sending email"
-                >
-                  Send Email
-                </Button>
-                <Modal modalState="signin" />
-              </Stack>
-            </form>
-          </Box>
+  return (
+    <Flex minH="60vh" align="center" justify="center" borderRadius="2xl">
+      <Stack spacing={8} mx="auto" maxW="lg" py={10} px={{ base: 0, sm: 6 }}>
+        <Stack align="center">
+          <Heading fontSize="3xl" align="center">
+            Forgot your password?
+          </Heading>
+          <Text fontSize="lg" color="gray.600">
+            You'll get an email with a reset link
+          </Text>
         </Stack>
-      </Flex>
+        <Box rounded="lg" bg={bgHook} boxShadow="lg" p={8}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={6}>
+              <FormControl id="password">
+                <FormLabel htmlFor="password">New Password</FormLabel>
+                <Input
+                  autoComplete="new-password"
+                  type="password"
+                  {...register('password', {
+                    required: 'Password is required',
+                    minLength: {
+                      value: 8,
+                      message: 'Password must be at least 8 characters long',
+                    },
+                    pattern: {
+                      value:
+                        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+                      message:
+                        'Must Contain One Uppercase, One Lowercase, One Number and One Special Case Character',
+                    },
+                  })}
+                />
+                {errors.password && (
+                  <FormValidationMsg errorMsg={errors.password.message} />
+                )}
+              </FormControl>
+              <FormControl id="passwordRepeat">
+                <FormLabel htmlFor="passwordRepeat">
+                  Repeat New Password
+                </FormLabel>
+                <Input
+                  autoComplete="new-password"
+                  type="password"
+                  {...register('passwordRepeat', {
+                    required: 'Password is required',
+                    validate: {
+                      matches: (value: string) =>
+                        value === password || 'Passwords do not match',
+                    },
+                  })}
+                />
+                {errors.passwordRepeat && (
+                  <FormValidationMsg errorMsg={errors.passwordRepeat.message} />
+                )}
+              </FormControl>
+
+              <Button
+                type="submit"
+                bg="blue.400"
+                color="white"
+                _hover={{
+                  bg: 'blue.500',
+                }}
+                disabled={!isValid || !isDirty || isSubmitting}
+                isLoading={isSubmitting}
+                loadingText="Sending email"
+              >
+                Send Email
+              </Button>
+              <Modal modalState="signin" />
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
     </Flex>
   );
 };
